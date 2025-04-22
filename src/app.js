ca8d1bd183b187687dc0115760fda077e36bce27
Warapong Thongkhundam <nat.n.dragon@gmail.com>
2025-04-22 15:15:35 +0700

    update app.js and app.test.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,14 @@ const dayOfTheWeek = (date = new Date()) => {
 
     return days[date.getDay()];
 }
+const monthOfTheYear = (date = new Date()) => {
+    const month = ['January', 'Februrary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    return month[date.getMonth()];
+
+}
+
+
 
 
 try {
@@ -11,3 +19,4 @@ try {
 
 
 exports.dayOfTheWeek = dayOfTheWeek;
+exports.monthOfTheYear = monthOfTheYear;
